8f57bc0300d3a88ccfc25b53aae289b70a0c988b
Naux334 <xinyuan.he334@gmail.com>
2022-04-07 20:36:41 +0100

    create functions on the c file

--- twitter_create.c
+++ twitter_create.c
@@ -17,3 +17,31 @@ void create_twitter_system(twitter * twitter_system){
     twitter_system-> users[0].num_followers = 0;
     twitter_system->users[0].num_following = 0;
 }
+
+void createUsers(){
+    printf("Test for createUsers success.\n");
+}
+
+void postTweets(){
+    printf("Test for postTweets success.\n");
+}
+
+void followUsers(){
+    printf("Test for followUSers success.\n");
+}
+
+void unfollowUsers(){
+    printf("Test for unfollowUsers success.\n");
+}
+
+void deleteUser(){
+    printf("Test for deleteUSer success.\n");
+}
+
+void endTurn(){
+    printf("Test for endTurn success.\n");
+}
+
+void endTwitter(){
+    printf("Test for endTwitter success.\n");
+}
