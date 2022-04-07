eb1319a75ccc6c4deda9763d7c397c6eea40d7ab
Naux334 <xinyuan.he334@gmail.com>
2022-04-07 16:35:59 +0100

    cancell the comment in the main.c

--- main.c
+++ main.c
@@ -10,10 +10,10 @@ int main() {
     printf("Followers: %d\n",twitter_system.users[0].num_followers);
     printf("Following: %d\n",twitter_system.users[0].num_following);
 
-//    for(int i =0; i < twitter_system.num_users; i++){
-//        user usr = twitter_system.users[i];
-//        printf("User: %s; Followers: %d; Following: %d\n",usr.username, usr.num_followers, usr.num_followers );
-//    }
+    for(int i =0; i < twitter_system.num_users; i++){
+        user usr = twitter_system.users[i];
+        printf("User: %s; Followers: %d; Following: %d\n",usr.username, usr.num_followers, usr.num_followers );
+    }
     //This is a print to show all the information when creating the users
     return 0;
     //implement here the code to print the users
